eb8b8d1f0c245fcbde341746586585c8feebe00d
DanilSizz <theghost.teenager@gmail.com>
2025-05-22 13:52:18 +0300

    Engine works fine

--- anim.py
+++ anim.py
@@ -1,15 +1,9 @@
 #Библиотека time
-import time
 import math
 
 class Anim:
   
-  def __init__(self, fps, draw):
-    
-    #Интервал обновления экрана
-    self.interval = 1 / fps 
-    #Таймер (объект класса Time)
-    self.time = time.time()
+  def __init__(self, draw):
     #Объект класса Draw
     self.draw = draw
     
@@ -20,19 +14,9 @@ class Anim:
       return ball
          
   #Функция для анимации объектов на экране
-  def anim(self, ball):
+  def update(self, ball):
 
-    self.draw.setColor("red")
+    self.draw.setColor("Black")
     self.draw.setPenSize(10)
-    while True:
-      
-      elapsed_time = time.time() - self.time
-  
-      if elapsed_time < self.interval:
-          time.sleep(self.interval - elapsed_time)
-          
-      print(ball.radius)
-      #self.draw.screen.clear()
-      self.pulsing_circle(ball)         
-      self.draw.circle(ball)
-      self.draw.display.update_screen() ###
+    self.draw.circle(ball)
+    self.draw.display.update_screen() ###
--- engine.py
+++ engine.py
@@ -1,14 +1,37 @@
+import time
+from anim import *
+
 class Engine:
 
-    def __init__(self, renderer):
-        pass
+    def __init__(self, renderer: Anim, fps):
+
+        self.renderer = renderer
+        #Интервал обновления экрана
+        self.interval = 1 / fps
+        #Таймер (объект класса Time)
+        self.time = time.time()
+        self.object = []
 
     def startGame(self):
-        pass
+        self.gameCycle()
 
-    def addObject(self):
-        pass
+    def addObject(self, object):
+        self.object.append(object)
 
     def gameCycle(self):
-        pass
 
+        i = 0
+        while True:
+
+            self.renderer.update(self.object[0])
+      
+            elapsed_time = time.time() - self.time
+  
+            if elapsed_time < self.interval:
+                time.sleep(self.interval - elapsed_time)
+
+            i = i + 1
+            print(i)
+
+
+    
